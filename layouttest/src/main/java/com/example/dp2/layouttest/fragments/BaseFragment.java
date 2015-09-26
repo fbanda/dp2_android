7e42010581a01b10a1f898392d191a708f994dc8
Fernando <fernandobanda1992@gmail.com>
2015-09-25 20:17:29 -0500

    b

---
+++ layouttest/src/main/java/com/example/dp2/layouttest/fragments/BaseFragment.java
@@ -0,0 +1,11 @@
+package com.example.dp2.layouttest.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Fernando on 25/09/2015.
+ */
+public class BaseFragment extends Fragment {
+
+
+}
